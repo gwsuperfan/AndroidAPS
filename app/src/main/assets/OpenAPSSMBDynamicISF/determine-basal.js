8fd7037cd1f7469ef78f3eb61a432ae93ed50a3f
Chris Wilson <chris@glucosegenius.com>
2022-04-09 18:16:12 -0700

    Updating function for DynISF to 1800/55 model with BG floor

--- app/src/main/assets/OpenAPSSMBDynamicISF/determine-basal.js
+++ app/src/main/assets/OpenAPSSMBDynamicISF/determine-basal.js
@@ -132,7 +132,12 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
     var bgTime = new Date(glucose_status.date);
     var minAgo = round( (systemTime - bgTime) / 60 / 1000 ,1);
 
-    var bg = glucose_status.glucose;
+    var rawbg = glucose_status.glucose;
+    if (rawbg<60) {
+        var bg = 60;
+        } else {
+        var bg = rawbg;
+        }
     var noise = glucose_status.noise;
     // 38 is an xDrip error state that usually indicates sensor failure
     // all other BG values between 11 and 37 mg/dL reflect non-error-code BG values, so we should zero temp for those
@@ -278,7 +283,7 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
         var dynISFadjust = profile.DynISFAdjust;
         var dynISFadjust = ( dynISFadjust / 100 );
         var TDD = (dynISFadjust * TDD);
-        var variable_sens = (1800 / ( TDD * Math.log((bg/75)+1)));
+        var variable_sens = (1800 / ( TDD * Math.log(bg/55)));
         variable_sens = round(variable_sens,1);
         if (dynISFadjust > 1 ) {
             console.log("TDD adjustment factor is: " +dynISFadjust+"; ");
@@ -808,20 +813,19 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
          console.log("EventualBG is" +eventualBG+" ;");
 
          if (bg > target_bg && glucose_status.delta < 3 && glucose_status.delta > -3 && glucose_status.short_avgdelta > -3 && glucose_status.short_avgdelta < 3 && eventualBG > target_bg && eventualBG < bg ) {
-             var future_sens = ( 1800 / (TDD * Math.log((((eventualBG * 0.5) + (bg * 0.5) )/75) +1 )
-              ));
+             var future_sens = ( 1800 / (TDD * Math.log(((eventualBG * 0.5) + (bg * 0.5) )/55)));
                  console.log("Future state sensitivity is " +future_sens+" based on eventual and current bg due to flat glucose level above target");
                  rT.reason += "Dosing sensitivity: " +future_sens+" using eventual BG;";
          }
 
          else if( glucose_status.delta > 0 && eventualBG > target_bg ) {
-             var future_sens = ( 1800 / (TDD * Math.log((bg/75)+1) ));
+             var future_sens = ( 1800 / (TDD * Math.log(bg/55) ));
              console.log("Future state sensitivity is " +future_sens+" using current bg due to small delta or variation");
              rT.reason += "Dosing sensitivity: " +future_sens+" using current BG;";
              }
 
          else {
-             var future_sens = ( 1800 / (TDD * Math.log((eventualBG/75)+1) ));
+             var future_sens = ( 1800 / (TDD * Math.log(eventualBG/55) ));
          console.log("Future state sensitivity is " +future_sens+" based on eventual bg due to -ve delta");
          rT.reason += "Dosing sensitivity: " +future_sens+" using eventual BG;";
          }
